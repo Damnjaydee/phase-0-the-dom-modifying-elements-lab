38e209baceffca888c7d603069354aff6be96c92
Damnjaydee <111096084+Damnjaydee@users.noreply.github.com>
2023-12-09 19:18:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Your JavaScript code
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id attribute to 'victory'
+newHeader.id = "victory";
+
+// Set the text content with your name
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new <h1> to the body or another appropriate element
+document.body.append(newHeader);
